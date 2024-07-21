bcd81779ba01fd9235fa607038d1cefa30abb403
AsamehELsayed <abdelahman2003.12.12@gmail.com>
2024-07-21 16:36:33 +0300

    initial commit

---
+++ src/components/BaseModel.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div class="w-screen h-screen left-0 top-0 bg-black opacity-30 absolute -z-10" @click='close'></div>
+  <div class="z-10  container absolute rounded-md bg-white  p-10 border  w-1/2 text-3xl top-1/4 left-1/4">
+    <slot></slot>
+     </div>
+</template>
+<script>
+export default {
+name:'BaseModel',
+methods:{
+    close(){
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="bg-blue-300 ">
+  <nav class=" container m-auto items-center flex flex-row  w-full h-16">
+    <div>
+    <h1 class="text-white text-3xl"><i class="pi pi-sun
+ pi-spin"></i>
+      Weather</h1>
+    </div>
+    <div class="flex flex-1 gap-1 text-white text-2xl justify-end"> 
+      <i class=" pi pi-info-circle" @click="open"/>
+      <i class="pi pi-plus"></i>
+    </div>
+  </nav>
+  </header>
+</template>
+
+<script >
+export default {
+  name: 'Navbar',
+  methods:{
+    open(){
+      this.$emit('open')
+    }
+  }
+}
+</script>
